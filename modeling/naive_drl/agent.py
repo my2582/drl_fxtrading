b60e843cb6067a0072a1ca30327aed37bd510f73
Minsu Yeom <my2582@columbia.edu>
2019-11-06 10:17:50 -0500

    another baseline is added.

--- modeling/naive_drl/agent.py
+++ modeling/naive_drl/agent.py
@@ -4,6 +4,19 @@ import tensorflow as tf
 import tensorflow.keras.optimizers as kop
 import tensorflow.keras.losses as kls
 
+class PortfolioManagement:
+    def __init__(self, M=9, P0=10000):
+        '''
+        Parameters:
+            M(int): the number of assets (excluding cash)
+            P0(int): an initial portfolio value (=usually in cash)
+        '''
+
+        self.pf_wt = np.zeros((1, M+1))
+        self.pf_wt[0][0] = 1    # cash 100% at time 0
+
+        
+
 class SimpleDRLAgent:
     def __init__(self, model, env, config, action_set = [-1, 0, 1], init_wealth = 10000, mode='train', verbose=False, test_model_path = None):
         self.config = config
@@ -32,8 +45,6 @@ class SimpleDRLAgent:
         self.state = None
         self.verbose = verbose
 
-
-
     def _compute_loss(self, obs_history, immed_rewards):
         return -tf.reduce_sum(immed_rewards)
 
@@ -56,17 +67,17 @@ class SimpleDRLAgent:
         return action
 
     def get_immed_reward(self, buysell, cost_bid, cost_ask, trade_bid, trade_ask):
-        cost_mid_price = (cost_bid + cost_ask)/2
-        trade_mid_price = (trade_bid + trade_ask)/2
+        # cost_mid_price = (cost_bid + cost_ask)/2
+        # trade_mid_price = (trade_bid + trade_ask)/2
         
-        if trade_mid_price > cost_mid_price:
+        if trade_bid > cost_bid:
             # Price is up
             if buysell == 'B':
                 # our action is correct in terms of betting direction
                 reward = 1
             else:
                 reward = -1
-        elif trade_mid_price < cost_mid_price:
+        elif trade_bid < cost_bid:
             # Price is down
             if buysell == 'B':
                 # wrong in betting direction
@@ -100,6 +111,10 @@ class SimpleDRLAgent:
         # Average rewards per epoch
         epochs_total_rewards = []
 
+        
+        # mean reversion agent
+        mean_rev_lookback = 20
+
         # Training loop
         for epoch in range(self.config['epochs']):
             for epi_idx in range(self.config['total_no_epi']):
@@ -111,6 +126,8 @@ class SimpleDRLAgent:
                 delta_wt = np.zeros((self.config['epi_sz'],))
                 immed_rewards = np.zeros((self.config['epi_sz'],))
                 accum_rewards = np.zeros((self.config['epi_sz'],))
+                mean_rev_accum_rewards = np.zeros((self.config['epi_sz'],))
+
 
                 # action_history = np.empty((self.config['epi_sz'],))
                 obs_history = np.zeros((self.config['epi_sz'],) + (self.config['obs_sz'],))
@@ -194,6 +211,22 @@ class SimpleDRLAgent:
                     ### Accumulate rewards within this episode.
                     accum_rewards[t] = accum_rewards[t-1] + immed_rewards[t]
 
+
+                    ####### mean reversion trading #########
+                    if t >= mean_rev_lookback:
+                        mean = np.average(cur_bid[t-mean_rev_lookback:t])
+                        std = np.std(cur_bid[t-mean_rev_lookback:t])
+                        z_score = (cur_bid[t-1] - mean)/std
+                        if z_score < -2:
+                            mean_rev_bs = 'B'
+                        elif z_score > 2:
+                            mean_rev_bs = 'S'
+                    else:
+                        mean_rev_bs = 'N'
+
+                    mean_rev_immed_rewards = self.get_immed_reward(mean_rev_bs, cur_bid[t-1], cur_ask[t-1], next_bid[0], next_ask[0])
+                    mean_rev_accum_rewards[t] = mean_rev_accum_rewards[t-1] + mean_rev_immed_rewards
+                    ########################################
                     if t % 100 == 0:
                         if self.verbose:
                             print('Time {}. Action(rec_wt) is {}. Reward is {}. Bid price goes from {} to {}'.format(t, action, immed_rewards[t], cur_bid[t-1], next_bid[0]))
@@ -201,9 +234,160 @@ class SimpleDRLAgent:
                 ### end of all batches ###
                 losses = self.model.train_on_batch(obs_history, immed_rewards)
 
-                print('Epoch:{} Episode:{}. The training loss is {}. The total reward is {}'.format(epoch+1, epi_idx+1, losses, accum_rewards[t]))
+                print('Epoch:{} Episode:{}. The training loss is {}. The DRL total reward is {} vs A baseline total reward is {}. DRL - Baseline = {}'.format(epoch+1, epi_idx+1, losses, accum_rewards[t], mean_rev_accum_rewards[t], accum_rewards[t] - mean_rev_accum_rewards[t]))
             
             ### enf of one epoch ##
             epochs_total_rewards.append(accum_rewards[-1])
         
-        return accum_rewards, np.array(epochs_total_rewards)
+        return accum_rewards, mean_rev_accum_rewards
+
+
+    def val(self):
+        # We keep `inv_wt` within this agent class as an attribute.
+        self.inv_wt = np.zeros((self.config['epi_sz'],))
+
+        # Within a batch, for time stamp t we have:
+        rec_wt = np.zeros((self.config['epi_sz'],))
+        delta_wt = np.zeros((self.config['epi_sz'],))
+        immed_rewards = np.zeros((self.config['epi_sz'],))
+        accum_rewards = np.zeros((self.config['epi_sz'],))
+
+        # action_history = np.empty((self.config['epi_sz'],))
+        obs_history = np.zeros((self.config['epi_sz'],) + (self.config['obs_sz'],))
+        
+        # Accumucated rewards per episode
+        # epi_rewards = []
+
+        # Average rewards per epoch
+        epochs_total_rewards = []
+
+        
+        # mean reversion agent
+        mean_rev_lookback = 20
+
+        # Training loop
+        for epoch in range(self.config['epochs']):
+            for epi_idx in range(self.config['total_no_epi']):
+                # We keep `inv_wt` within this agent class as an attribute.
+                self.inv_wt = np.zeros((self.config['epi_sz'],))
+
+                # Within a batch, for time stamp t we have:
+                rec_wt = np.zeros((self.config['epi_sz'],))
+                delta_wt = np.zeros((self.config['epi_sz'],))
+                immed_rewards = np.zeros((self.config['epi_sz'],))
+                accum_rewards = np.zeros((self.config['epi_sz'],))
+                mean_rev_accum_rewards = np.zeros((self.config['epi_sz'],))
+
+
+                # action_history = np.empty((self.config['epi_sz'],))
+                obs_history = np.zeros((self.config['epi_sz'],) + (self.config['obs_sz'],))
+
+                # We start with 0% weight in the target currency.
+                
+                t, state, price_tuple, is_finished = self.env.reset(epi_idx)
+
+                #  - rec_wt[t]: the recommended investment weight by nn
+                #  - delta_wt[t]: a difference in weights between the recommended investment weight by nn
+                #                 and current weight in the target currency.
+                #  - rewards[t]: rewards from the latest recommended trade (caued by the recommended weight)
+                # features = state[:-2]
+                rec_wt[t] = state[-2]
+                self.inv_wt[t] = rec_wt[t]
+                delta_wt[t] = state[-1]
+                # immed_rewards[t] = 0.
+                # accum_rewards[t] = 0.
+                
+                cur_bid, cur_ask, _, _ = price_tuple
+
+                # We have observed and not taken any action yet.
+                obs = np.append(t, state)
+                obs_history[t] = obs
+
+                # action_history is a list of recommneded weights
+                # We start with zero weight
+                action_history = [0.]
+
+                # buysell_history is a list of 'B'(buy), 'S'(sell), 'N' (No trade)
+                buysell_history = ['N']
+
+                ### start of batch ###
+                # We loop through time stamps within an episode.
+                while not is_finished:
+                    # The neural networks take observation history and action history as inputs
+                    # and they return an action-like scalar [-1,1].
+                    # This scalar is a recommended investment weight (rec_wt) in the target currency.
+                    # `state` is a numpy array (features, rec_wt, delta_wt)
+                    # The nueral networks are called during self.act() being executed
+                    action = self.act(obs_history, action_history)
+                    action_history.append(action)
+
+                    t, state, price_tuple, is_finished = self.env.step(action, self.inv_wt[t])
+                    # features = state[:-2]
+                    rec_wt[t] = state[-2]
+                    delta_wt[t] = state[-1]
+                    obs = np.append(t, state)
+                    obs_history[t] = obs
+
+                    # Since we ran 'env.stop()', step counter increased by 1.
+                    # Therefore, price_tuple[0] is interpreted as next_bid
+                    next_bid, next_ask, _, _ = price_tuple
+
+                    ### Trading begins
+                    # buy/sell decision
+                    # buysell = 'B' if delta_wt[t] >=0 else 'S'
+                    # if delta_wt[t] > 0:
+                    #     buysell = 'B'
+                    # elif delta_wt[t] == 0:
+                    #     buysell = 'N'
+                    # else:
+                    #     buysell = 'S'
+                    if action > 0:
+                        buysell = 'B'
+                    elif action == 0:
+                        buysell = 'N'
+                    else:
+                        buysell = 'S'
+                    buysell_history.append(buysell)
+
+                    # change an investment weight
+                    self.inv_wt[t] = self.inv_wt[t-1] + delta_wt[t]
+
+                    # profit/loss calculation
+                    # pass
+
+                    ### Get an immedidate reward
+                    immed_rewards[t] = self.get_immed_reward(buysell, cur_bid[t-1], cur_ask[t-1], next_bid[0], next_ask[0])
+
+                    ### Accumulate rewards within this episode.
+                    accum_rewards[t] = accum_rewards[t-1] + immed_rewards[t]
+
+
+                    ####### mean reversion trading #########
+                    if t >= mean_rev_lookback:
+                        mean = np.average(cur_bid[t-mean_rev_lookback:t])
+                        std = np.std(cur_bid[t-mean_rev_lookback:t])
+                        z_score = (cur_bid[t-1] - mean)/std
+                        if z_score < -2:
+                            mean_rev_bs = 'B'
+                        elif z_score > 2:
+                            mean_rev_bs = 'S'
+                    else:
+                        mean_rev_bs = 'N'
+
+                    mean_rev_immed_rewards = self.get_immed_reward(mean_rev_bs, cur_bid[t-1], cur_ask[t-1], next_bid[0], next_ask[0])
+                    mean_rev_accum_rewards[t] = mean_rev_accum_rewards[t-1] + mean_rev_immed_rewards
+                    ########################################
+                    if t % 100 == 0:
+                        if self.verbose:
+                            print('Time {}. Action(rec_wt) is {}. Reward is {}. Bid price goes from {} to {}'.format(t, action, immed_rewards[t], cur_bid[t-1], next_bid[0]))
+
+                ### end of all batches ###
+                # losses = self.model.train_on_batch(obs_history, immed_rewards)
+
+                print('Epoch:{} Episode:{}. The DRL total reward is {} vs A baseline total reward is {}. DRL - Baseline = {}'.format(
+                    epoch+1, epi_idx+1, accum_rewards[t], mean_rev_accum_rewards[t], accum_rewards[t] - mean_rev_accum_rewards[t]))
+            
+            ### enf of one epoch ##
+            epochs_total_rewards.append(accum_rewards[-1])
+        
+        return accum_rewards, mean_rev_accum_rewards
--- modeling/naive_drl/run.py
+++ modeling/naive_drl/run.py
@@ -28,25 +28,27 @@ logger = logging.getLogger(__name__)
 
 config = {}
 config['lag'] = 10
-config['epi_sz'] = 90
+config['epi_sz'] = 180
 config['split_sz'] = config['epi_sz'] + config['lag']
-config['total_no_epi'] = 910  # floor(91057/1460), 91057 == # rows of any currency.
+config['total_no_epi'] = 50  # floor(91057/1460), 91057 == # rows of any currency.
 config['lr_rate'] = 0.001
 config['momentum'] = 0.9
 config['target_currency'] = 'usdjpy'
 config['epochs'] = 1
-config['features_sz'] = 2*config['epi_sz']  # It's features.shape[1] from "_, _, features = draw_episode(..)"
+# config['features_sz'] = 2*config['epi_sz']  # It's features.shape[1] from "_, _, features = draw_episode(..)"
 config['buffer_sz'] = 50000
 config['batch_sz'] = 64
 config['num_batches'] = 100
 config['start_learning'] = 5000
+config['M'] = 9     # The number of currency pairs to invest in.
 
 # First '1' is a space for 'timestamp'
 # Last '2' is a spcae for [rec_wt, delta_wt]
-config['obs_sz'] = 1 + config['features_sz'] + 2
+config['obs_sz'] = 1 + config['epi_sz'] + 2
 tf.keras.backend.set_floatx('float32')
 
 X_train = pd.read_csv('./dataset/toy_X_train.csv', sep=',')
+X_val = pd.read_csv('./dataset/toy_X_val.csv', sep=',')
 assert config['total_no_epi']*config['split_sz'] <= np.min([X_train[key].shape[0] for key in X_train.keys()]), "Training set has less data points than # of episodes * # of splits."
 
 
@@ -60,6 +62,10 @@ action_set = [0,1,2]
 model = SimpleModel(action_set)
 env = Environment(X_train, config, num_agents=1)
 simple_agent = SimpleDRLAgent(model, env, config, verbose=False)
-epi_rewards, epochs_avg_rewards = simple_agent.train()
-print(epi_rewards, epochs_avg_rewards)
+epi_rewards, epi_rewards_mean_rev = simple_agent.train()
+print(epi_rewards, epi_rewards_mean_rev)
 
+env_val = Environment(X_val, config, num_agents=1)
+simple_agent_val = SimpleDRLAgent(model, env_val, config, verbose=False)
+epi_rewards_val, epi_rewards_mean_rev_val = simple_agent_val.val()
+print(epi_rewards_val, epi_rewards_mean_rev_val)
